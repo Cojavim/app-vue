39b12b13900df9297f49a6240d9af7e157ef0d67
tomas.valasek <tomas.valasek@ingeteam.com>
2020-11-18 12:53:49 +0100

    fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="home">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <v-btn block @click="test" > Load data </v-btn>
+    <v-btn block @click="loadDataFromServer" > Load data </v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
   components: {
     // HelloWorld,
   },
   methods: {
-    test() {
-      console.log("test");
-    },
+    async loadDataFromServer(){
+          const response = await fetch("http://localhost:5000/WeatherForecast/temperatures")
+          console.log(await response.json())
+      }
   },
 };
 </script>
